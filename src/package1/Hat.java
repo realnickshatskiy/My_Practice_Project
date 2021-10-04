eaca16322ead21ff6d9ed1c108f728541a67413b
realnickshatskiy <real.nickshatskiy@gmail.com>
2021-10-04 15:48:45 -0400

    updated file Hat in package1

--- src/package1/Hat.java
+++ src/package1/Hat.java
@@ -3,5 +3,9 @@ package package1;
 public class Hat {
     public static void main(String[] args) {
         System.out.println("Red Hat");
+
+        //adding some lines
+        //for new commit
+
     }
 }
