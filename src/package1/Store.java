c243da8a1b4a2d2a8c64974fd85bec5a2c2dbc31
realnickshatskiy <real.nickshatskiy@gmail.com>
2021-10-04 15:56:28 -0400

    updated file Store in package1

--- src/package1/Store.java
+++ src/package1/Store.java
@@ -3,5 +3,11 @@ package package1;
 public class Store {
     public static void main(String[] args) {
         System.out.println("We in the store");
+
+        // here some more lines
+
+        // updating for new commit
+
+        // let's see what's up
     }
 }
